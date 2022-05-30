173227151b2a61d420412067a2886d72fddaad93
lanegrete <lanegrete@STFMXC1682>
2022-05-30 16:52:44 -0500

    Creando clase Main

---
+++ src/test/java/Eloisa.java
@@ -0,0 +1,13 @@
+import java.io.IOException;
+
+public class Eloisa {
+
+	public static void main(String[] args) throws InterruptedException, IOException {
+		
+		Robot r = new Robot();
+		
+		r.test_PruebaCompleta();
+
+	}
+
+}
